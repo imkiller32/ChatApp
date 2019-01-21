607831ed4467fb31b81beebcda16b104b76a9f6b
Ritesh Agarwal <riteshagg50297@gmail.com>
2019-01-22 01:20:27 +0530

    init

---
+++ lib/chat_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'chat_message.dart';
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+
+  final List<ChatMessage> _messages=<ChatMessage>[];
+  final TextEditingController _textController = new TextEditingController();
+
+  void _handleSubmitted(String text){
+    _textController.clear();
+    ChatMessage message=ChatMessage(
+      text: text,
+    );
+    setState(() {
+          _messages.insert(0, message);
+        });
+  }
+
+  Widget _textComposerWidget(){
+    return IconTheme(
+      data:IconThemeData(color: Colors.blue),
+          child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+          Flexible(
+            child: TextField(
+            decoration: InputDecoration.collapsed(hintText: "Send a message"),
+            controller: _textController,
+            onSubmitted: _handleSubmitted,
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 4.0),
+            child: IconButton(
+              icon: Icon(Icons.send),
+              onPressed: ()=>_handleSubmitted(_textController.text),
+            ),
+          ),
+        ],),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              padding: EdgeInsets.all(8.0),
+              reverse: true,
+              itemBuilder: (_,int index)=>_messages[index],
+              itemCount: _messages.length,
+            ),
+          ),
+          Divider(
+            height: 1.0,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardColor,
+            ),
+            child: _textComposerWidget(),
+          ),
+        ],
+      );
+  }
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'chat_screen.dart';
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("ProjectBattein"),),
+      body: ChatScreen(),
+    );
+  }
+}
